3a3fa1b78ed867cf0a06cdc172ced1be8aa5a1fb
Alevivaldi <alevivaldibr02@gmail.com>
2023-11-22 08:31:00 -0300

    ok

---
+++ contaBanco/src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SistemaBancario sistema = new SistemaBancario();
+
+        System.out.println("Digite seu nome ");
+        sistema.setNomeCliente(sc.nextLine());
+
+        System.out.println("Por favor, digite o número da Agência !");
+        sistema.setAgencia(sc.nextLine());
+
+        System.out.println("Por favor, digite o número da conta !");
+        sistema.setNumero(sc.nextInt());
+
+        System.out.println("Digite o valor Desejado para Deposito ");
+        sistema.setSaldo(sc.nextDouble());
+                       
+
+        System.out.println("Olá "+sistema.getNomeCliente()+", obrigado por criar uma conta em nosso banco, sua agência é "+
+        sistema.getAgencia()+", conta "+sistema.getNumero()+" e seu saldo "+sistema.getSaldo()+" já está disponível para saque.");
+        sc.close();
+    }
+}
---
+++ contaBanco/src/SistemaBancario.java
@@ -0,0 +1,34 @@
+public class SistemaBancario {
+    private int numero = 0;
+    private String NomeCliente = null;
+    private String agencia = null;
+    private double saldo = 0.0;
+    
+   
+
+
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public String getNomeCliente() {
+        return NomeCliente;
+    }
+    public void setNomeCliente(String NomeCliente) {
+        this.NomeCliente = NomeCliente;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo += saldo;
+    }
+}
